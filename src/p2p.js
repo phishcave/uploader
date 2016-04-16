0589e43287925c8ced092eb85d404218d199da53
Dylan Johnston <qdylanj@gmail.com>
2016-04-16 17:50:54 +0200

    p2pwip

---
+++ src/p2p.js
@@ -0,0 +1,106 @@
+var RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+var RTCSessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription;
+
+var P2P = function(sessionID, callbacks) {
+  var config = { 'iceServers': [{'urls': 'stun:23.21.150.121'}] };
+  var conArgs = { 'optional': [{'DtlsSrtpKeyAgreement': true}] };
+  var connection = new RTCPeerConnection(config, conArgs);
+  var connected = false;
+  var data = null; // wrapper arounder sender / receiver.
+
+  this.connected = function() {
+    return connected;
+  };
+
+  this.createAnswer = function() {
+    data.createAnswer();
+  };
+
+  this.create = function() {
+    data = new P2PSender(connection, callbacks);
+  };
+
+  this.receive = function() {
+    data = new P2PReceiver(connection, sessionID, callbacks);
+  };
+};
+
+var P2PComponent = function(sessionID) {
+  var container = div({cls: 'status'});
+  var data = null;
+  var p2p = null;
+
+  var init = function() {
+    var callbacks = {
+      connect: onConnect.bind(this),
+      disconnect: onDisconnect.bind(this),
+      sessionExists: onSessionExists.bind(this),
+      sessionNotFound: onSessionNotFound.bind(this)
+    };
+
+    p2p = new P2P(sessionID, callbacks);
+
+    if (!isSender()) {
+      p2p.receive();
+    }
+  };
+
+  var onSessionExists = function() {
+    console.log("sessionExists");
+    p2p.createAnswer();
+  };
+
+  var onSessionNotFound = function() {
+    console.log("sessionDoesNotExist");
+    H.empty(container);
+    container.appendChild(div("Session does not exist!"));
+  };
+
+  var onConnect = function() {
+    console.log("Connect");
+  };
+
+  var onDisconnect = function() {
+    console.log("Disconnect");
+  };
+
+  var isSender = function() {
+    return sessionID.length == 0;
+  };
+
+  var drawStatus = function() {
+    H.empty(container);
+
+    if ( data.connected() ) {
+      container.appendChild(div('connected'));
+    } else {
+      container.appendChild(div('disconnected'));
+    }
+  };
+
+  var onSendClick = function() {
+    console.log('send click');
+    p2p.create();
+  };
+
+  var onReceiveClick = function() {
+    console.log('receive click');
+    p2p.receive();
+  };
+
+  var sendButton = div({onclick: onSendClick.bind(this), cls: 'btn'}, "Send File...");
+  var receiveButton = div({onclick: onReceiveClick.bind(this), cls: 'btn'}, "Receive");
+
+  this.render = function() {
+    if (isSender()) {
+      container.appendChild(sendButton);
+    } else {
+      container.appendChild(
+        div('Attempting to receive file with id ' + sessionID)
+      )
+    }
+    return container;
+  };
+
+  init();
+};
